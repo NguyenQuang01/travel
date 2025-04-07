a7839be229b7b542b7325edce311c2501007b2db
Quangnt <thangteoxx@gmail.com>
2025-04-07 18:14:28 +0700

    refactor(slider): remove autoplay and improve text overflow handling
    The autoplay feature was removed from the Swiper component to reduce unnecessary distractions. Additionally, text overflow handling was improved for the item name and description to ensure better readability and layout consistency.

--- src/app/components/sectionHome/slider.tsx
+++ src/app/components/sectionHome/slider.tsx
@@ -25,79 +25,75 @@ interface SliderItem {
 
 const Slider = (prop: { data: SliderItem[] }) => {
     return (
-        <div className="bg-black flex items-center justify-center text-[#333] leading-6 my-0 mb-[118px] py-[30px] pb-10 md:h-[444px] h-[344px]">
-            <div>
-                <p className="text-[1.8rem] font-bold text-shadow-md text-[#fff] text-center mb-[80px]">
-                    Choose your travel style
-                </p>
-                <div className="mx-auto md:w-[70vw]  w-[83vw] max-w-[126rem] h-full relative md:-mb-[11.8rem] -mb-[14.8rem]">
-                    <Swiper
-                        modules={[Navigation, Pagination, Autoplay]}
-                        breakpoints={{
-                            320: {
-                                slidesPerView: 3,
-                                spaceBetween: 10,
-                            },
-                            768: {
-                                slidesPerView: 5,
-                                spaceBetween: 30,
-                            },
-                        }}
-                        navigation
-                        autoplay={{
-                            delay: 3000,
-                            disableOnInteraction: false,
-                        }}
-                        loop={true}
-                        className="h-full"
-                    >
-                        {prop.data.map((item) => (
-                            <SwiperSlide key={item.id}>
-                                <Link href={"/guide/" + item.name}>
-                                    <div className="flex items-center justify-center h-full">
-                                        <div className="card">
-                                            {/* <h1 className="text-h1 w-full">
-                                            {item.name}
-                                        </h1> */}
-                                            <Image
-                                                src={
-                                                    item.imageUrl
-                                                        ? `${API_INFO.BASE_URL_ADMIN}${item.imageUrl}`
-                                                        : gereralImg
-                                                }
-                                                alt={item.name}
-                                                width={201}
-                                                height={44}
-                                                priority={true}
-                                                className="min-w-[12.6rem] h-[44px]"
-                                                loading="eager"
-                                                style={imgStyle}
-                                            />
-                                            <div
-                                                className="intro"
-                                                style={{
-                                                    backgroundColor: `hsl(${
-                                                        Math.random() * 360
-                                                    }, 70%, 50%)`,
-                                                }}
-                                            >
-                                                <h2 className="text-h2">
-                                                    {item.name}
-                                                </h2>
-                                                <p className="text-p">
-                                                    {item.description ||
-                                                        "No description available"}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-            </div>
+      <div className="bg-black flex items-center justify-center text-[#333] leading-6 my-0 mb-[118px] py-[30px] pb-10 md:h-[444px] h-[344px]">
+        <div>
+          <p className="text-[1.8rem] font-bold text-shadow-md text-[#fff] text-center mb-[80px]">
+            Choose your travel style
+          </p>
+          <div className="mx-auto md:w-[70vw]  w-[83vw] max-w-[126rem] h-full relative md:-mb-[11.8rem] -mb-[14.8rem]">
+            <Swiper
+              modules={[Navigation, Pagination]}
+              breakpoints={{
+                320: {
+                  slidesPerView: 3,
+                  spaceBetween: 10,
+                },
+                768: {
+                  slidesPerView: 5,
+                  spaceBetween: 30,
+                },
+              }}
+              navigation
+              //   autoplay={{
+              //     delay: 3000,
+              //     disableOnInteraction: false,
+              //   }}
+              loop={true}
+              className="h-full"
+            >
+              {prop.data.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <Link href={"/guide/" + item.name}>
+                    <div className="flex items-center justify-center h-full">
+                      <div className="card">
+                        <Image
+                          src={
+                            item.imageUrl
+                              ? `${API_INFO.BASE_URL_ADMIN}${item.imageUrl}`
+                              : gereralImg
+                          }
+                          alt={item.name}
+                          width={201}
+                          height={44}
+                          priority={true}
+                          className="min-w-[12.6rem] h-[44px]"
+                          loading="eager"
+                          style={imgStyle}
+                        />
+                        <div
+                          className="intro"
+                          style={{
+                            backgroundColor: `hsl(${
+                              Math.random() * 360
+                            }, 70%, 50%)`,
+                          }}
+                        >
+                          <h2 className="text-h2 text-start whitespace-nowrap overflow-hidden text-ellipsis">
+                            {item.name}
+                          </h2>
+                          <p className="text-p line-clamp-4 overflow-hidden text-ellipsis whitespace-normal h-26">
+                            {item.description || "No description available"}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
+      </div>
     );
 };
 
